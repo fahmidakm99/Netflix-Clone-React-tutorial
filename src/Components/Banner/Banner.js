188be990bef82b25ae80b926f5fd2d8a6b3ac0b4
fahmida <fanihahaanifa@gmail.com>
2026-03-06 16:43:49 +0400

    added big trailer for banner

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,24 +1,56 @@
 import React, { useEffect, useState } from "react";
 import "./Banner.css";
+import YouTube from "react-youtube";
 import axios from "../axios";
 import { API_KEY, imageUrl } from "../../constants/constants";
 
 function Banner() {
   const [movie, setMovie] = useState();
+  const [trailerKey, setTrailerKey] = useState(""); // YouTube video key
+  const [showModal, setShowModal] = useState(false); // modal toggle
 
   useEffect(() => {
     axios
       .get(`trending/all/week?api_key=${API_KEY}&language=en-US`)
       .then((response) => {
-        console.log(response.data.results);
-        // setMovie(response.data.results[0])
         const movies = response.data.results;
-
         const randomMovie = movies[Math.floor(Math.random() * movies.length)];
-
         setMovie(randomMovie);
       });
   }, []);
+
+  const opts = {
+    height: "480",
+    width: "854",
+    playerVars: {
+      autoplay: 1,
+      origin: window.location.origin,
+      rel: 0,
+    },
+  };
+
+  const handlePlay = () => {
+    if (!movie) return;
+
+    const mediaType = movie.media_type === "tv" ? "tv" : "movie";
+
+    axios
+      .get(`/${mediaType}/${movie.id}/videos?api_key=${API_KEY}&language=en-US`)
+      .then((response) => {
+        if (response.data.results.length > 0) {
+          setTrailerKey(response.data.results[0].key);
+          setShowModal(true); // open modal
+        } else {
+          console.log("No trailer available for this movie.");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    setTrailerKey("");
+  };
 
   return (
     <div
@@ -28,15 +60,28 @@ function Banner() {
       className="banner"
     >
       <div className="content">
-        <h1 className="title">{movie ? movie.title : ""} </h1>
+        <h1 className="title">{movie ? movie.title || movie.name : ""}</h1>
         <div className="banner_buttons">
-          <button className="button">Play</button>
+          <button className="button" onClick={handlePlay}>
+            Play
+          </button>
           <button className="button">My list</button>
-          {/* https://jsonplaceholder.typicode.com/posts */}
-          {/* https://netflix-clone-react-tutorial.vercel.app/ */}
         </div>
         <h1 className="description">{movie ? movie.overview : ""}</h1>
       </div>
+
+      {/* Modal for trailer */}
+      {showModal && (
+        <div className="trailer-modal">
+          <div className="trailer-content">
+            <button className="close-btn" onClick={closeModal}>
+              X
+            </button>
+            <YouTube videoId={trailerKey} opts={opts} />
+          </div>
+        </div>
+      )}
+
       <div className="fade_bottom"></div>
     </div>
   );
